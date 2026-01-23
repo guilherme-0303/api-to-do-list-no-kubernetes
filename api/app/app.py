07eeb6270bb75a0bcf9b0fd87b9ac480f4d3496a
Guilherme Santos <guilherme.0303@debian.com>
2026-01-23 16:17:20 -0300

    cria o arquivo app.py e inclui nele a rota todos

---
+++ api/app/app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+
+from app.routers import todos
+
+
+app = FastAPI()
+
+
+# Setting up the routers
+app.include_router(todos.router)
